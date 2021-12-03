e3adb00b8cc0a2b2c70b66527fa0048fd26adf4f
Ziad <ziadhany2016@gmail.com>
2021-12-03 14:18:13 +0200

    master

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,7 +2,15 @@ function handleSubmit(event) {
     event.preventDefault()
     let formText = document.getElementById('name').value
     if (Client.checkURL(formText)){
-    fetch('http://localhost:8080/test?url='+formText)
+    fetch('http://localhost:8080/test',
+    {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({ "url" : formText})
+    })
     .then(response => response.json())
     .then(function(res) {
         console.log(res)
--- src/server/index.js
+++ src/server/index.js
@@ -5,13 +5,14 @@ var path = require('path')
 const express = require('express')
 const axios = require('axios');
 var cors = require('cors')
+var bodyParser = require('body-parser')
 
 console.log(`Your API key is ${process.env.API_KEY}`);
-
 const app = express()
-
 app.use(express.static('dist'))
 app.use(cors())
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
 console.log(__dirname)
 
 app.get('/', function (req, res) {
@@ -22,8 +23,8 @@ app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-app.get('/test', function (req, res) {
-    var url = req.query.url;
+app.post('/test', function (req, res) {
+    var url = req.body.url;
     const api = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&url=${url}&lang=en`;
     axios.get(api).then(function (response) {
         res.send(response.data)
